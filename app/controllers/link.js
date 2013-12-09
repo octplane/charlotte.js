cb0989cc2e0a7c6ee6f6bc916dde9d097710c6d8
Pierre Baillet <pierre@baillet.name>
2013-12-10 00:01:01 +0100

    More CSS...

--- app/controllers/link.js
+++ app/controllers/link.js
@@ -9,35 +9,31 @@ exports.post_link = function(req, res) {
 };
 
 exports.add = function(req, res) {
-	request(req.query.url, function(error, response, body) {
-		if (!error && response.statusCode == 200) {
-			var $ = cheerio.load(body);
-			console.log($("title").text());
-	 		res.render('link/add', { 
-	 			url: req.query.url, 
-	 			url_title: $("title").text(),
-	 			in_add_sequence: true
-	 		});
-		} else {
-			console.log(error);
-		}
-	});
-};
-
-exports.post = function(req, res) {
-	if (!req.query.title)
+	if (!req.query.title) {
 		request(req.query.url, function(error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var $ = cheerio.load(body);
 				console.log($("title").text());
-		 		res.render('link/add', {
-		 			url: req.query.url,
+		 		res.render('link/add', { 
+		 			url: req.query.url, 
 		 			url_title: $("title").text(),
-		  			in_add_sequence: true });
+		 			tags: "",
+		 			description: "",
+		 			in_add_sequence: true
+		 		});
 			} else {
 				console.log(error);
 			}
 		});
-	res.render('link/add', { url: req.query.url, url_title: req.query.title});
-	
+	} else {
+		res.render('link/add', { url: req.query.url, tags:"", in_add_sequence: true, description: "", url_title: req.query.title});
+	}
+};
+
+exports.post = function(req, res) {
+	var parms = req.body;
+	console.log(parms);
+	var post_identifier = "plop";
+    res.redirect('/', {highlight: post_identifier});
+
 };
