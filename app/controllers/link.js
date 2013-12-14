fb3c6774ece3779f55a74f89b6f6e9f2bc976c9f
Pierre Baillet <pierre@baillet.name>
2013-12-14 13:02:56 +0100

    WIP save

--- app/controllers/link.js
+++ app/controllers/link.js
@@ -1,8 +1,21 @@
 var db = require("./db"),
 	cheerio = require('cheerio'),
-	request = require('request');
+	request = require('request'),
+	crc32 = require('crc32'),
+	btoa = require('btoa');
+    
+smallHash = function(text) {
+	var crc = crc32(text), bytes = [], hash;
 
-
+	// This implementation seems to be crc32b but I don't care
+	// http://stackoverflow.com/questions/15861058/what-is-the-difference-between-crc32-and-crc32b
+	for(var p=0; p < crc.length; p+=2) {
+		bytes.push(parseInt(crc.substr(p, 2), 16));
+	}
+	
+	hash = btoa(String.fromCharCode.apply(String, bytes)).replace(/=*$/,'');
+	return hash;
+}
 
 exports.post_link = function(req, res) {
 	res.render('link/post_link');
@@ -32,7 +45,7 @@ exports.add = function(req, res) {
 
 exports.post = function(req, res) {
 	var parms = req.body;
-	console.log(parms);
+	console.log(smallHash(req.body));
 	var post_identifier = "plop";
     res.redirect('/', {highlight: post_identifier});
 
